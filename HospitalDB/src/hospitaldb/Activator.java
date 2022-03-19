904d4823d56409be6cd93a5bb7040a0456d0afa9
DESKTOP-D7AKHEM\Rajindu <rajinducooray98se@gmail.com>
2022-03-19 10:48:02 +0530

    Admission pro & con

--- HospitalDB/src/hospitaldb/Activator.java
+++ HospitalDB/src/hospitaldb/Activator.java
@@ -2,21 +2,21 @@ package hospitaldb;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
+	private ServiceRegistration serviceRegistration;
 
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+		System.out.println("Hospital DB started");
+		HospitalDatabase database = new HospitalDatabaseImpl();
+		serviceRegistration = bundleContext.registerService(HospitalDatabase.class.getName(), database, null);
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		System.out.println("Hospital DB stopped");
+		serviceRegistration.unregister();
 	}
 
 }
